0b732fa113bbfb694e849ec5db441db33a7fb607
Yogesh More <yogeshm0143@gmail.com.com>
2023-01-24 23:23:50 +0530

    Added : Pattern 5 program.

---
+++ src/Pattern_5.java
@@ -0,0 +1,10 @@
+public class Pattern_5 {
+    public static void main(String args[]) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 5; j >= 1; j--) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+    }
+}
